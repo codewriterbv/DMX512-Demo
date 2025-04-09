aef53f1c3542a161710c79c259a395650cc9912d
Frank Delporte <fdelporte@azul.com>
2025-04-09 09:47:07 +0200

    Test with library to show serial ports

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# DMX512-Demo
-Demo JavaFX application using the DMX512 library
+# DMX512 Demo
+
+Demo JavaFX application using the [DMX512 Java library](https://github.com/codewriterbv/DMX512).
--- src/main/java/be/codewriter/dmx512demo/DMX512DemoApp.java
+++ src/main/java/be/codewriter/dmx512demo/DMX512DemoApp.java
@@ -1,18 +1,23 @@
 package be.codewriter.dmx512demo;
 
+import be.codewriter.dmx512.controller.DMXIPController;
+import be.codewriter.dmx512.controller.DMXSerialController;
+import be.codewriter.dmx512.serial.SerialConnection;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import javafx.util.StringConverter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class DMX512DemoApp extends Application {
     private static final Logger LOGGER = LogManager.getLogger(DMX512DemoApp.class.getName());
-    
+
     public void run() {
         LOGGER.info("Starting UI...");
         launch();
@@ -20,7 +25,28 @@ public class DMX512DemoApp extends Application {
 
     @Override
     public void start(Stage stage) {
-        var holder = new HBox(new Label("Test"));
+        var dmxSerialController = new DMXSerialController();
+        var dmxIpController = new DMXIPController();
+
+        var serialConnections = new ComboBox<SerialConnection>();
+        serialConnections.getItems().addAll(dmxSerialController.getAvailablePorts());
+        serialConnections.setConverter(new StringConverter<SerialConnection>() {
+            @Override
+            public String toString(SerialConnection serialConnection) {
+                return serialConnection != null ? serialConnection.description() : ""; // Show name field
+            }
+
+            @Override
+            public SerialConnection fromString(String string) {
+                return dmxSerialController.getAvailablePorts()
+                        .stream()
+                        .filter(p -> p.description().equals(string))
+                        .findFirst()
+                        .orElse(null);
+            }
+        });
+
+        var holder = new HBox(new Label("Test"), serialConnections);
         holder.setPadding(new Insets(10));
         holder.setSpacing(10);
 
